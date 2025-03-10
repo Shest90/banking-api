80377050afd174bf14aabbd482304549848fc00b
Andrey Shestopalov <andruha19905@gmail.com>
2025-03-10 23:52:28 +0500

    fix/RegisterRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,37 +2,24 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     // Регистрация пользователя
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'first_name'            => 'required|string|max:255',
-            'last_name'             => 'required|string|max:255',
-            'email'                 => 'required|string|email|max:255|unique:users',
-            'phone_number'          => 'nullable|string',
-            'password'              => 'required|string|min:6|confirmed',
-        ]);
+        $data = $request->validated();
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
+        $data['password'] = Hash::make($data['password']);
 
-        $user = User::create([
-            'first_name'   => $request->first_name,
-            'last_name'    => $request->last_name,
-            'email'        => $request->email,
-            'phone_number' => $request->phone_number,
-            'password'     => Hash::make($request->password),
-        ]);
+        $user = User::create($data);
 
         $token = JWTAuth::fromUser($user);
 
